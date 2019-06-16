072a06c9185c7c4821a168a5185c4f2be8a87adf
Florencia Nodar <fnodar@algeiba.com>
2019-06-16 19:31:32 -0300

    se agregó el archivo configuracion.properties y se modificó el mail con el archivo de configuracion

--- TP_Reflection/src/Main.java
+++ TP_Reflection/src/Main.java
@@ -1,11 +1,32 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import Servicios.Consultas;
 import Utilidades.UConexion;
 
 
 public class Main {
-  public static void main(String[] args) {
-        
-        UConexion.setUrl("jdbc:sqlserver://cleanarchitecturedemo.database.windows.net;database=Reflection;user=sqladmin;password=P@assword1;");
+  public static void main(String[] args){
+	  	
+		try {
+			Properties prop=new Properties();
+		  	FileInputStream ip;
+			ip = new FileInputStream("src/framework.properties");
+			try {
+				prop.load(ip);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        UConexion.setUrl(prop.getProperty("url"));
+
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	  	
         Persona persona = (Persona) Consultas.obtenerPorId(Persona.class, 1);
         System.out.print(persona);       
 
